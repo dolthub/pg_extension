a0f512531065d2baecf18af81f7e7f93b0f1c94d
Daylon Wilkins <daylon@liquidata.co>
2025-07-01 07:23:37 -0700

    Changed how DLL loading on Windows works

--- library/build_definitions_windows.ps1
+++
@@ -1,3 +0,0 @@
-$link = "${env:VSINSTALLDIR}VC\Tools\MSVC\*\bin\Hostx64\x64\link.exe"
-$link = Get-Command $link | Select-Object -First 1
-& $link /DLL /NOENTRY /DEF:postgres.def /OUT:../output/postgres.exe
